bfb370e6e02b3ba7ba55e6d61c2dfe80fe81ba0a
zahraesmaeeli <z.esmaeelabadia@gmail.com>
2023-10-01 22:45:10 +0330

    first commit

---
+++ lambda-exercise.py
@@ -0,0 +1,27 @@
+# l=[8,7,4,9,3,10,2]
+# l.sort()
+# print(l)
+#
+#
+#
+# def Dec(func):
+#     print("HaHaHa i am hacker" )
+#
+#
+#
+# Dec("HaHaHa i am hacker")
+# def myprint(man):
+#     print(man)
+#     # return myprint
+#
+# man=("Some Text from SabzDanesh.com")
+# myprint(man)
+
+
+
+matrix=[[1,2,3,4],[9,10,11,12],[13,14,15,16]]
+# T=[[row[i]for row in matrix]for i in range(4) ]
+# print(T)
+# or:
+matrix_reverse=zip(*matrix)
+print(list (matrix_reverse))
